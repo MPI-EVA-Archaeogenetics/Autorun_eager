285f285daf0baa25eec1258c98fd1c00dd2b843a
Thiseas Christos Lamnidis <thisseass@gmail.com>
2021-11-09 16:39:58 +0100

    Now works by sequencing batch.

--- prepare_eager_tsv.R
+++ prepare_eager_tsv.R
@@ -20,31 +20,58 @@ validate_analysis_type <- function(option, opt_str, value, parser) {
                                                        "'\nAccepted values: ", paste(valid_entries,collapse=", "),"\n\n"))
 }
 
+## Save one eager input TSV per individual. Rename if necessary. Input is already subset data.
+save_ind_tsv <- function(data, rename, output_dir, ...) {
+  # print(rename)
+  # print(output_dir)
+  ## Infer Individual Id from input.
+  ind_id <- data %>% select(Sample_Name) %>% distinct() %>% pull()
+  # print(ind_id)
+  if (rename) {
+    data <- data %>% mutate(Library_ID=str_replace_all(Library_ID, "[.]", "_")) %>% ## Replace dots in the Library_ID to underscores.
+      select(Sample_Name, Library_ID,  Lane, Colour_Chemistry, 
+             SeqType, Organism, Strandedness, UDG_Treatment, R1, R2, BAM)
+  }
+  ind_dir <- paste0(output_dir,"/",ind_id)
+  # print(ind_dir)
+  write(paste0("Creating output directory '",ind_dir,"'"), stdout())
+  dir.create(ind_dir, showWarnings = F, recursive = T) ## Create output directory and subdirs if they do not exist.
+  readr::write_tsv(data, file=paste0(ind_dir,"/",ind_id,".tsv")) ## Output structure can be changed here.
+}
 ## MAIN ##
 
 ## Parse arguments ----------------------------
 parser <- OptionParser(usage = "%prog [options] .credentials")
-parser <- add_option(parser, c("-i", "--input_iid"), type = 'character', 
-                     action = "store", dest = "input_iid", 
-                     help = "The Pandora individual ID of the input individual. A TSV file will be prepared with all relevant processed BAM files from this\n\t\tindividual")
+parser <- add_option(parser, c("-s", "--sequencing_batch_id"), type = 'character', 
+                     action = "store", dest = "sequencing_batch_id", 
+                     help = "The Pandora sequencing batch ID to update eager input for. A TSV file will be prepared for each individual in this run, containing all relevant processed BAM files from the\n\t\tindividual")
 parser <- add_option(parser, c("-a", "--analysis_type"), type = 'character',
                      action = "callback", dest = "analysis_type",
                      callback = validate_analysis_type, default=NA,
-                     help="The analysis type to compile the data from. Should be one of: 'SG', 'TF'.")
-parser <- add_option(parser, c("-r", "--rename"), type='logical',
-                     action='store_true', dest='rename', default=F,
-                     help="Changes all dots (.) in the Library_ID field of the output to underscores (_).
+                     help = "The analysis type to compile the data from. Should be one of: 'SG', 'TF'.")
+parser <- add_option(parser, c("-r", "--rename"), type = 'logical',
+                     action = 'store_true', dest = 'rename', default=F,
+                     help = "Changes all dots (.) in the Library_ID field of the output to underscores (_).
 			Some tools used in nf-core/eager will strip everything after the first dot (.)
 			from the name of the input file, which can cause naming conflicts in rare cases."
                      )
+parser <- add_option(parser, c("-o", "--outDir"), type = 'character',
+                     action = "store", dest = "outdir",
+                     help= "The desired output directory. Within this directory, one subdirectory will be created per individual ID, and one TSV within each directory."
+                     )
+                     
 arguments <- parse_args(parser, positional_arguments = 1)
 
 opts <- arguments$options
 cred_file <- arguments$args
 
-
-tibble_input_iid <- tibble("Ind"=opts$input_iid)
+sequencing_batch_id <- opts$sequencing_batch_id
 analysis_type <- opts$analysis_type
+
+if (is.na(analysis_type)) {
+  stop(call.=F, "\nNo analysis type provided.\n")
+}
+output_dir <- opts$outdir
 
 #############
 ## PANDORA ##
@@ -61,8 +88,10 @@ complete_pandora_table <- join_pandora_tables(
   )
 ) %>% convert_all_ids_to_values(., con = con)
 
+tibble_input_iids <- complete_pandora_table %>% filter(sequencing.Batch == sequencing_batch_id) %>% select(individual.Full_Individual_Id)
+
 ## Pull information from pandora, keeping only matching IIDs and requested Sequencing types.
-results <- inner_join(complete_pandora_table, tibble_input_iid, by=c("individual.Full_Individual_Id"="Ind")) %>%
+results <- inner_join(complete_pandora_table, tibble_input_iids, by=c("individual.Full_Individual_Id"="individual.Full_Individual_Id")) %>%
   filter(grepl(paste0("\\.", analysis_type), sequencing.Full_Sequencing_Id)) %>%
   select(individual.Full_Individual_Id,individual.Organism,library.Full_Library_Id,library.Protocol,analysis.Result_Directory,sequencing.Sequencing_Id) %>%
   distinct() %>%
@@ -91,17 +120,8 @@ results <- inner_join(complete_pandora_table, tibble_input_iid, by=c("individual
      "R2",
      "BAM"
     )
-if (opts$rename) {
-  cat(
-    format_tsv(results %>% 
-                 mutate(Library_ID=str_replace_all(Library_ID, "[.]", "_")) %>% ## Replace dots in the Library_ID to underscores.
-                 select(Sample_Name, Library_ID,  Lane, Colour_Chemistry, 
-                        SeqType, Organism, Strandedness, UDG_Treatment, R1, R2, BAM))
-  )
-} else {
-  cat(
-    format_tsv(results %>% 
-                 select(Sample_Name, Library_ID,  Lane, Colour_Chemistry, 
-                        SeqType, Organism, Strandedness, UDG_Treatment, R1, R2, BAM))
-  )
-}
+
+write_tsv(results, file="results.txt")
+
+## Group by individual IDs and save each chunk as TSV
+results %>% group_by(Sample_Name) %>% group_walk(~save_ind_tsv(., rename=F, output_dir=output_dir), .keep=T)
