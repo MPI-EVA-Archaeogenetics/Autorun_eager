d0bf10c78cf9ceda4c584418874d490318beb0f6
Thiseas Christos Lamnidis <thisseass@gmail.com>
2021-11-10 17:52:25 +0100

    No informative run names. Correct eager output dir.

--- run_Eager.sh
+++ run_Eager.sh
@@ -7,7 +7,7 @@ root_input_dir='/mnt/archgen/users/lamnidis/popgen_autoprocess/eager_inputs' ## 
 root_output_dir='/mnt/archgen/users/lamnidis/popgen_autoprocess/eager_outputs'
 
 ## Set base profiles for EVA cluster.
-nextflow_profiles="eva,archgen,autorun"
+nextflow_profiles="eva,archgen,medium_data,autorun"
 
 ## Set colour and face for colour printing
 Red='\033[1;31m'$(tput bold) ## Red bold face
@@ -21,9 +21,9 @@ for analysis_type in "SG" "TF"; do
     # echo "${root_input_dir}/${analysis_type}"
     for eager_input in ${root_input_dir}/${analysis_type}/*/*.tsv; do
         ## Set output directory name from eager input name
-        eager_output_dir="${root_output_dir}/$(basename ${eager_input} .tsv)"
-        ## Run name is individual ID followed by analysis_type
-        run_name="$(basename ${eager_input} .tsv)_${analysis_type}"
+        eager_output_dir="${root_output_dir}/${analysis_type}/(basename ${eager_input} .tsv)"
+        # ## Run name is individual ID followed by analysis_type
+        # run_name="$(basename ${eager_input} .tsv)_${analysis_type}"
         # echo $run_name
         ## If no multiqc_report exists (last step of eager), or TSV is newer than the report, start an eager run.
         #### Always running with resume will ensure runs are only ever resumed instead of restarting.
@@ -39,7 +39,7 @@ for analysis_type in "SG" "TF"; do
                 --outdir ${eager_output_dir} \
                 -w ${eager_output_dir}/work \
                 -with-tower -ansi-log false \
-                -resume ${run_name}"
+                -resume"
             
             ## Actually run eager now.
             nextflow run nf-core/eager \
@@ -51,7 +51,7 @@ for analysis_type in "SG" "TF"; do
                 --outdir ${eager_output_dir} \
                 -w ${eager_output_dir}/work \
                 -with-tower -ansi-log false \
-                -resume ${run_name}
+                -resume # ${run_name}
         fi
     done
 done
