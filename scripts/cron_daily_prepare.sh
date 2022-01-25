e2965dc78203c98ef3c427ed81447a8b5160b3cb
Thiseas C. Lamnidis <thisseass@gmail.com>
2022-01-25 12:25:48 +0100

    Correct path for prepare_eager_tsv.sh

--- scripts/cron_daily_prepare.sh
+++ scripts/cron_daily_prepare.sh
@@ -8,11 +8,11 @@ cd /mnt/archgen/Autorun_eager
 # 1240k
 # Note: this find only checks runs starting from 2020
 find /mnt/archgen/Autorun/Results/Human_1240k/2* -name "Results.txt" -mtime -1 | cut -f 7 -d "/"| sort -u| while read RUN ; do
-    prepare_eager_tsv.R -s $RUN -a TF -o eager_inputs/ -d .eva_credentials
+    scripts/prepare_eager_tsv.R -s $RUN -a TF -o eager_inputs/ -d .eva_credentials
 done 
 
 # Shotgun
 # Note: this find only checks runs starting from 2020
 find /mnt/archgen/Autorun/Results/Human_Shotgun/2* -name "Results.txt" -mtime -1 | cut -f 7 -d "/"| sort -u| while read RUN ; do
-    prepare_eager_tsv.R -s $RUN -a SG -o eager_inputs/ -d .eva_credentials
+    scripts/prepare_eager_tsv.R -s $RUN -a SG -o eager_inputs/ -d .eva_credentials
 done 
