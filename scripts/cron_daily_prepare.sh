210669d47c5129b2a15c4f4cc9e96c238c4d9d72
Thiseas C. Lamnidis <thisseass@gmail.com>
2023-06-07 13:18:51 +0200

    Fix debug messages

--- scripts/cron_daily_prepare.sh
+++ scripts/cron_daily_prepare.sh
@@ -9,13 +9,13 @@ cd /mnt/archgen/Autorun_eager
 # 1240k
 # Note: this find only checks runs starting from 2020
 find /mnt/archgen/Autorun/Results/Human_1240k/2* -name '*.bam' -mtime -1 | cut -f 7 -d "/"| sort -u| while read RUN ; do
-    echo "Processing SG data from run: ${RUN}"
+    echo "Processing TF data from run: ${RUN}"
     scripts/prepare_eager_tsv.R -s $RUN -a TF -o eager_inputs/ -d .eva_credentials
 done 
 
 # Shotgun
 # Note: this find only checks runs starting from 2020
 find /mnt/archgen/Autorun/Results/Human_Shotgun/2* -name '*.bam' -mtime -1 | cut -f 7 -d "/"| sort -u| while read RUN ; do
-    echo "Processing TF data from run: ${RUN}"
+    echo "Processing SG data from run: ${RUN}"
     scripts/prepare_eager_tsv.R -s $RUN -a SG -o eager_inputs/ -d .eva_credentials
 done 
