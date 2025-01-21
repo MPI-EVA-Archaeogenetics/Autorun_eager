da59c2b016261644061361e21123b23da1dc57bf
Thiseas C. Lamnidis <thisseass@gmail.com>
2025-01-21 15:21:37 +0100

    pick up YC results daily

--- scripts/cron_daily_prepare.sh
+++ scripts/cron_daily_prepare.sh
@@ -33,3 +33,10 @@ find /mnt/archgen/Autorun/Results/Human_RM/2* -name '*.bam' -mtime -1 2>/dev/nul
     echo "Processing RM data from run: ${RUN}"
     scripts/prepare_eager_tsv.R -s $RUN -a RM -o eager_inputs/ -d .eva_credentials
 done
+
+# Y + mtDNA capture (YMCA)
+# Note: this find only checks runs starting from 2020.  Silence stderr to avoid 'permission denied'.
+find /mnt/archgen/Autorun/Results/Human_Y/2* -name '*.bam' -mtime -1 2>/dev/null | cut -f 7 -d "/" | sort -u | while read RUN ; do
+    echo "Processing YC data from run: ${RUN}"
+    scripts/prepare_eager_tsv.R -s $RUN -a YC -o eager_inputs/ -d .eva_credentials
+done
