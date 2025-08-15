c0d709b9d31e354f90e606d02dd655cfd13d00d5
Thiseas Christos Lamnidis <thisseass@gmail.com>
2025-08-15 16:06:15 +0200

    add scp command note

--- singularity/build_singularity_with_docker.sh
+++ singularity/build_singularity_with_docker.sh
@@ -6,3 +6,4 @@ docker pull tclamnidis/singularity-in-docker:3.8.1
 docker run --rm --privileged -v $(pwd):/work tclamnidis/singularity-in-docker:3.8.1 build --force singularity/AE_R_deps_singularity.sif singularity/AE_R_deps_singularity.def
 
 ## The resulting image should then be moved to the appropriate directory on the server-side installation of Autorun_eager.
+## scp singularity/AE_R_deps_singularity.sif autoeager:/mnt/archgen/Autorun_eager/singularity/
