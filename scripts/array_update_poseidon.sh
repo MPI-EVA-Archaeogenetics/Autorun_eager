401236ff653eb27ecbb03ea332eb14d191cdc166
Thiseas Christos Lamnidis <thisseass@gmail.com>
2026-07-20 14:28:49 +0200

    now submits array job

--- scripts/array_update_poseidon.sh
+++ scripts/array_update_poseidon.sh
@@ -23,3 +23,7 @@ echo '' > ${output_fn}
 while read r; do
     echo "/mnt/archgen/Autorun_eager/scripts/update_poseidon_package_v2.sh -a ${analysis_type} ${r}" >> ${output_fn}
 done < ${ind_list_fn}
+
+jn=$(wc -l ${output_fn} | cut -f 1 -d " ") ## number of jobs equals number of lines
+echo "sbatch --mem=4GB -p short --cpus-per-task=1 --job-name=poseidon_spawner_${output_fn} --output=/mnt/archgen/Autorun_eager/.tmp/$(basename ${output_fn})/%x.po%A.%a --array 1-${jn} /mnt/archgen/Autorun_eager/scripts/submit_as_array.sh ${output_fn}"
+sbatch --mem=4GB -p short --cpus-per-task=1 --job-name=poseidon_spawner_${output_fn} --output=/mnt/archgen/Autorun_eager/.tmp/$(basename ${output_fn})/%x.po%A.%a --array 1-${jn} /mnt/archgen/Autorun_eager/scripts/submit_as_array.sh ${output_fn}
