e2a01c1440c1db971e199abf98f6c3fc98879364
Thiseas Christos Lamnidis <thisseass@gmail.com>
2026-07-21 16:35:43 +0200

    put logs in dedicated directory

--- scripts/submit_site_package_jobs.sh
+++ scripts/submit_site_package_jobs.sh
@@ -43,5 +43,5 @@ for a in ${root_poseidon_dir}/*; do
 done
 
 jn=$(wc -l < ${output_fn})
-echo "sbatch --mem=4GB -p short --cpus-per-task=1 --job-name=site_spawner_$(basename ${output_fn}) --output=/mnt/archgen/Autorun_eager/.tmp/sites/$(basename ${output_fn})/%x.po%A.%a --array 1-${jn} /mnt/archgen/Autorun_eager/scripts/submit_as_array.sh ${output_fn}"
-sbatch --mem=4GB -p short --cpus-per-task=1 --job-name=site_spawner_$(basename ${output_fn}) --output=/mnt/archgen/Autorun_eager/.tmp/sites/$(basename ${output_fn})/%x.po%A.%a --array 1-${jn} /mnt/archgen/Autorun_eager/scripts/submit_as_array.sh ${output_fn}
+echo "sbatch --mem=4GB -p short --cpus-per-task=1 --job-name=site_spawner_$(basename ${output_fn}) --output=/mnt/archgen/Autorun_eager/.tmp/sites/logs/$(basename ${output_fn})/%x.po%A.%a --array 1-${jn} /mnt/archgen/Autorun_eager/scripts/submit_as_array.sh ${output_fn}"
+sbatch --mem=4GB -p short --cpus-per-task=1 --job-name=site_spawner_$(basename ${output_fn}) --output=/mnt/archgen/Autorun_eager/.tmp/sites/logs/$(basename ${output_fn})/%x.po%A.%a --array 1-${jn} /mnt/archgen/Autorun_eager/scripts/submit_as_array.sh ${output_fn}
