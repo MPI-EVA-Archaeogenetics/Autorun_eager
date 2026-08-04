6deade4a578d9ee5380384da195f5adc021cf9cf
Thiseas C. Lamnidis <thisseass@gmail.com>
2026-08-04 17:12:01 +0200

    use scratch dir

--- scripts/submit_site_package_jobs.sh
+++ scripts/submit_site_package_jobs.sh
@@ -22,7 +22,8 @@ source /mnt/archgen/Autorun_eager/scripts/helper_functions.sh
 root_poseidon_dir="/mnt/archgen/internal_poseidon_archives"
 date_stamp="$(date -I)"
 trident_path="/r1/people/srv_autoeager/bin/trident-2.1.0.0"
-output_fn="/mnt/archgen/scratch/srv_autoeager/.sites/joblists/${date_stamp}_site_package_update_list.txt"
+scratch_dir="/mnt/archgen/scratch/srv_autoeager/.sites"
+output_fn="${scratch_dir}/joblists/${date_stamp}_site_package_update_list.txt"
 errecho -y "Creating joblist for site package updates. Output will be written to ${output_fn}."
 echo '' > ${output_fn}
 
@@ -43,5 +44,5 @@ for a in ${root_poseidon_dir}/*; do
 done
 
 jn=$(wc -l < ${output_fn})
-echo "sbatch -t 4:00:00 --mem=4GB -p short --cpus-per-task=1 --job-name=site_spawner_$(basename ${output_fn}) --output=/mnt/archgen/Autorun_eager/.tmp/sites/logs/$(basename ${output_fn})/%x.po%A.%a --array 1-${jn} /mnt/archgen/Autorun_eager/scripts/submit_as_array.sh ${output_fn}"
-sbatch -t 4:00:00 --mem=4GB -p short --cpus-per-task=1 --job-name=site_spawner_$(basename ${output_fn}) --output=/mnt/archgen/Autorun_eager/.tmp/sites/logs/$(basename ${output_fn})/%x.po%A.%a --array 1-${jn} /mnt/archgen/Autorun_eager/scripts/submit_as_array.sh ${output_fn}
+echo "sbatch -t 4:00:00 --mem=4GB -p short --cpus-per-task=1 --job-name=site_spawner_$(basename ${output_fn}) --output=${scratch_dir}/logs/$(basename ${output_fn})/%x.po%A.%a --array 1-${jn} /mnt/archgen/Autorun_eager/scripts/submit_as_array.sh ${output_fn}"
+sbatch -t 4:00:00 --mem=4GB -p short --cpus-per-task=1 --job-name=site_spawner_$(basename ${output_fn}) --output=${scratch_dir}/logs/$(basename ${output_fn})/%x.po%A.%a --array 1-${jn} /mnt/archgen/Autorun_eager/scripts/submit_as_array.sh ${output_fn}
