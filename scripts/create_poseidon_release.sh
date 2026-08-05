1e4eb4a901b4ac95e9e48c14cdaa2310b05cf1e2
Thiseas C. Lamnidis <thisseass@gmail.com>
2026-08-05 13:21:45 +0200

    rename to snapshots. run for one analysis per run.

--- scripts/create_poseidon_release.sh
+++ scripts/create_poseidon_release.sh
@@ -10,26 +10,27 @@ source /mnt/archgen/Autorun_eager/scripts/helper_functions.sh
 
 ## Helptext function
 function Helptext() {
-  echo -ne "\t usage: $0 [options] <release_name>\n\n"
-  echo -ne "This creates a dated release of all poseidon packages in each analysis type.\n\n"
+  echo -ne "\t usage: $0 [options]\n\n"
+  echo -ne "This creates a dated snapshot of all poseidon packages in the requested analysis type.\n\n"
   echo -ne "Options:\n"
-  echo -ne "-d, --packageDate\t\tOptional.The ISO formatted date the letter packages were created. Assumed to be today if not provided."
+  echo -ne "-a, --analysis_type\tThe analysis type for which a snapshot should be prepared.\n"
+  echo -ne "-d, --packageDate\tOptional.The ISO formatted date the letter packages were created. Assumed to be today if not provided.\n"
   echo -ne "-h, --help\t\tPrint this text and exit.\n"
   echo -ne "-v, --version \t\tPrint version and exit.\n"
 }
 
 ## Parse CLI args.
-TEMP=`getopt -q -o dhv --long packageDate,help,version -n 'create_poseidon_releases.sh' -- "$@"`  
+TEMP=`getopt -q -o a:d:hv --long analysis_type:,packageDate:,help,version -n 'create_poseidon_snapshot.sh' -- "$@"`  
 eval set -- "$TEMP"
 
 ## parameter defaults
 trident_path="/r1/people/srv_autoeager/bin/trident-2.1.0.0"
-scratch_dir='/mnt/archgen/scratch/srv_autoeager/.releases'
+scratch_dir='/mnt/archgen/scratch/srv_autoeager/.snapshots'
 root_poseidon_dir="/mnt/archgen/internal_poseidon_archives"
-release_dir="${root_poseidon_dir}/releases/"
+snapshot_dir="${root_poseidon_dir}/snapshots/"
 date_stamp="$(date -I)"
 input_date="${date_stamp}"
-dependency=''
+analysis=''
 
 ## Read in CLI arguments
 while true ; do
@@ -37,61 +38,69 @@ while true ; do
     -h|--help) Helptext; exit 0 ;;
     -v|--version) echo ${VERSION}; exit 0;;
     -d|--packageDate) input_date="$2"; shift 2;;
+    -a|--analysis_type) analysis="$2"; shift 2;;
     --) break ;;
     *) echo -e "invalid option provided: $1.\n"; Helptext; exit 1;;
   esac
 done
 
-joblist_fn="${scratch_dir}/.joblists/${date_stamp}_release.joblist"
-echo -n '' > ${joblist_fn} ##Flush out list if it exists.
+if [[ -z "${analysis}" ]]; then
+  errecho "No analysis type was provided. Valid analyses: SG TF TM RP RM."
+  exit 1
+fi
+## From helper_functions.sh
+validate_analysis_type ${analysis}
 
-for a in SG TF TM RP RM; do
-  TEMPDIR=$(mktemp -d ${scratch_dir}/${date_stamp}/release)
-  LOG=${TEMPDIR}/forge.log
-  ${trident_path} --logMode SimpleLog 
-    forge \
-    --outFormat EIGENSTRAT \
-    -d ${scratch_dir}/${input_date} \
-    -o ${TEMPDIR}/${a} \
-    2>&1 | tee -a ${LOG} ## Save stderr/stdout to log file for future reference.
-  
-  check_fail "Package Forge failed for package in: ${TEMPDIR}/${a}"
-  errecho -g "## Package Forge completed ##\n"
+errecho -y "## Forge Snapshot ##\n"
+mkdir -p ${scratch_dir}/${date_stamp}/
+TEMPDIR=$(mktemp -d ${scratch_dir}/${date_stamp}/snapshot_${analysis}_XXXX)
+LOG=${TEMPDIR}/forge.log
+${trident_path} --logMode SimpleLog forge \
+  --outFormat EIGENSTRAT \
+  -d ${scratch_dir}/${input_date}/${analysis} \
+  -o ${TEMPDIR}/${analysis} \
+  2>&1 | tee -a ${LOG} ## Save stderr/stdout to log file for future reference.
 
-  if [[ -f ${release_dir}/${a}/POSEIDON.yml ]]; then
-    ## If a release already exists, update it.
-    cp ${release_dir}/${a}/POSEIDON.yml ${TEMPDIR}/${a}/
-  fi
-  LOG=${TEMPDIR}/rectify.log
-  ${trident_path} --logMode SimpleLog rectify \
-    --checksumAll \
-    -d ${TEMPDIR}/${a} \
-    --logText "${a} release: ${date_stamp}" \
-    --packageVersion Major \
-    2>&1 | tee -a ${LOG} ## Save stderr/stdout to log file for future reference.
+check_fail $? "Package Forge failed for package in: ${TEMPDIR}/${analysis}"
+errecho -g "## Package Forge completed ##\n"
 
-  check_fail $? "Trident rectify command failed for package in: ${TEMPDIR}/${a}."
-  errecho -g "## Package Rectify completed ##\n"
+if [[ -f ${snapshot_dir}/${analysis}/POSEIDON.yml ]]; then
+  errecho -y "## Copying Existing YAML for package ##\n"
+  ## If a snapshot already exists, update it.
+  cp ${snapshot_dir}/${analysis}/POSEIDON.yml ${TEMPDIR}/${analysis}/
+fi
+errecho -y "## Rectify Snapshot ##\n"
+LOG=${TEMPDIR}/rectify.log
+${trident_path} --logMode SimpleLog rectify \
+  --checksumAll \
+  -d ${TEMPDIR}/${analysis} \
+  --logText "${analysis} snapshot: ${date_stamp}" \
+  --packageVersion Major \
+  --newContributors '[Thiseas C. Lamnidis](thiseas_christos_lamnidis@eva.mpg.de);[Kay Pruefer](kay_pruefer@eva.mpg.de)' \
+  2>&1 | tee -a ${LOG} ## Save stderr/stdout to log file for future reference.
 
-  LOG=${TEMPDIR}/validate.log
-  ${trident_path} --logMode SimpleLog validate \
-    -d ${TEMPDIR}/${a} \
-    2>&1 | tee -a ${LOG} ## Save stderr/stdout to log file for future reference.
-  
-  check_fail $? "Trident validation command failed for package in: ${TEMPDIR}/${a}."
-  errecho -g "## Package validation completed ##\n"
+check_fail $? "Trident rectify command failed for package in: ${TEMPDIR}/${analysis}."
+errecho -g "## Package Rectify completed ##\n"
 
-  ## Move release to live directory
-  mkdir -p ${release_dir}
-  if [[ -d ${release_dir}/${a} ]]; then
-    errecho "[${0##*/}]: Removing old package at '${release_dir}/${a}/'"
-    rm -f ${release_dir}/${a}/*
-    rmdir ${release_dir}/${a}/
-  fi
-  errecho "[${0##*/}]: Publishing package to '${release_dir}/${a}'"
-  mv ${TEMPDIR}/${a} ${release_dir}/${a}
-  check_fail $? "[${0##*/}]: Failed to publish package to '${release_dir}/${a}'"
-  errecho -g "## Package Publish completed ##\n"
-  errecho "[${0##*/}]: Removing temporary directory '${TEMPDIR}'"
-  rm -rf ${TEMPDIR}
-done
+errecho -y "## Validate Snapshot ##\n"
+LOG=${TEMPDIR}/validate.log
+${trident_path} --logMode SimpleLog validate \
+  -d ${TEMPDIR}/${analysis} \
+  2>&1 | tee -a ${LOG} ## Save stderr/stdout to log file for future reference.
+
+check_fail $? "Trident validation command failed for package in: ${TEMPDIR}/${analysis}."
+errecho -g "## Package validation completed ##\n"
+
+## Move snapshot to live directory
+mkdir -p ${snapshot_dir}
+if [[ -d ${snapshot_dir}/${analysis} ]]; then
+  errecho "[${0##*/}]: Removing old package at '${snapshot_dir}/${analysis}/'"
+  rm -f ${snapshot_dir}/${analysis}/*
+  rmdir ${snapshot_dir}/${analysis}/
+fi
+errecho "[${0##*/}]: Publishing package to '${snapshot_dir}/${analysis}'"
+mv ${TEMPDIR}/${analysis} ${snapshot_dir}/${analysis}
+check_fail $? "[${0##*/}]: Failed to publish package to '${snapshot_dir}/${analysis}'"
+errecho -g "## Package Publish completed ##\n"
+errecho "[${0##*/}]: Removing temporary directory '${TEMPDIR}'"
+rm -rf ${TEMPDIR}
